fcb72afe0dd43d439328e22903caaa460360942d
Ryan <mugoryan2@gmail.com>
2023-10-21 05:33:37 -0700

    correction made

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 /**
  * main - Entry point
  *
- * return: always 1 (success)
+ * Return: Always 1 (success)
  */
-int main (void)
+int main(void)
 {
 	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
+
 	write(2, str1, 59);
 	return (1);
 }
