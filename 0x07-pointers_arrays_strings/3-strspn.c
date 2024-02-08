ca42956bfcb844c3ffea31257ecc186587883486
Ryan <githaigaraizzy@gmail.com>
2024-02-08 00:51:48 -0800

    Corrected task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -19,7 +19,7 @@ unsigned int _strspn(char *s, char *accept)
 		flag = 0;
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] ==accept[j])
+			if (s[i] == accept[j])
 			{
 				f++;
 				flag = 1;
